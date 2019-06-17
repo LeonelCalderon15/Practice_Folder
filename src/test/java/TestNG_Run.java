5633e90eb7e8b081e2b5ed691102c6e52eb7cab6
LeonelCalderon15 <calderoncodeproject.com>
2019-06-17 14:46:40 +0800

    Signed-off-by: LeonelCalderon15 <calderoncodeproject.com>

---
+++ src/test/java/TestNG_Run.java
@@ -0,0 +1,18 @@
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class TestNG_Run {
+  @Test
+  public void f() {
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+}
